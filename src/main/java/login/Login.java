994b19518fc326b2b5b0934cb3b9c2ad8a297bc3
meigocupcake <smstephanie@outlook.com>
2026-02-20 21:38:04 -0300

    Redirecionamento de página de login

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -5,10 +5,12 @@
 package login;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
@@ -16,10 +18,26 @@ import java.io.IOException;
  */
 // ctrl+espaço p atalho
 //servidor
+@WebServlet("/login")
 public class Login extends HttpServlet{
+    
+    private static final long serialVersionUID = 1L;
+    
     //enviando - pego a função e reconheça como (request)
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String usuario = request.getParameter("users");
+        String senha = request.getParameter("passw");
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        if("admin".equals(usuario) && "1234".equals(senha)){
+            response.sendRedirect("login.html");
+            // out.println("<h2>Login realizado!</h2>");
+        } else {
+                out.println("<h2>Usuário ou senha incorreto!");
+            }
+        
     }
 }
